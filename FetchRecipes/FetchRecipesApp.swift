9aef1dd747a894a3224dbca27efe96eaf7b6da86
frncg <101316054+frncg@users.noreply.github.com>
2025-06-06 09:04:53 -0400

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Franco Miguel Guevarra on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipesTests/FetchRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesTests.swift
+//  FetchRecipesTests
+//
+//  Created by Franco Miguel Guevarra on 6/6/25.
+//
+
+import Testing
+@testable import FetchRecipes
+
+struct FetchRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
